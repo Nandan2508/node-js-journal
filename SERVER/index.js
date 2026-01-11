0e88f8a291519e9e3eaae7b7ca6dbfb2878a558e
Nandan Verma <nandan.nv358@gmail.com>
2026-01-11 16:20:41 +0530

    Learnt Express Js

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,25 +1,12 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
-const myServer = http.createServer((req,res)=>{
-    if(req.url=== '/favicon.ico') return res.end();
-    const log = `${Date.now()} : ${req.url}: New Req Recieved\n`;
-    const myUrl = url.parse(req.url,true);
-    console.log(myUrl);
-    fs.appendFile('log.txt',log , (err,data)=>{
-        switch(myUrl.pathname){
-            case('/') : res.end('hello from home')
-            break;
-            case('/about') :
-            const username = myUrl.query.name ;
-             res.end(`hi ${username}`);
-            break;
-            case('/contact') : res.end('hello from contact')
-            break;
-            case('/meow'): res.end('hello from meow')
-        }
-    })
-    //console.log(req.headers);
-    //res.end('Hello from Serv')
-});
-myServer.listen(8000,()=>console.log("Server Started"));
+const express = require('express');
+const app = express();
+app.get('/',(req,res)=>{
+    return res.send(`hello from home page , Hey ${req.query.name} , you are ${req.query.age} years old `);
+})
+app.get('/about',(req,res)=>{
+    return res.send("This is about page ");
+})
+app.listen(8000,()=>console.log("server is running on port 8000"));
